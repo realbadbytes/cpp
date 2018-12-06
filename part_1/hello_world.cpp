83dfd7ed9b5865bfd68a72f19bb0c9a0590aaf7a
realbadbytes <badbytes.io@gmail.com>
2018-12-05 16:22:19 -0800

    some cleanup

--- part_1/hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    /* The << says "write the second argument to the first argument", 
-     * in this case, a string to the stdout stream
-     */
-    std::cout << "hello world\n";
-}
