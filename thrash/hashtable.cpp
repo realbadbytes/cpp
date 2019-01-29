18346854a5cb958596db4f076d1250157cc79dd9
realbadbytes <badbytes.io@gmail.com>
2019-01-28 21:44:11 -0800

    lcamtuf AFL style spacing

--- thrash/hashtable.cpp
+++ thrash/hashtable.cpp
@@ -7,10 +7,15 @@ using namespace std;
 
 
 /* Hash table entry struct to support closed addressing */
+
 struct TableEntry {
+
     TableEntry* next;
+
     string s;
+
     int data;
+
 };
 
 
@@ -27,33 +32,49 @@ struct TableEntry {
  * A second solution is closed addressing where each spot in the hash table can 
  * hold a linked list of items that matched that hash code.
  */
+
 int hash_func(TableEntry *t)
 {
+
     int hash_value = 0;
+
     string s = t->s;
 
     for (int c : s) {
+
         /* Using int gets the ascii code */
+
         hash_value += c;
+
     }
+
     /* Divide by number of elements in the hash table, save remainder as hash code
      * aka modulus 
      */
+
     cout << "hash of \"" << s << "\" is " << hash_value % 5 << endl;
+
     return hash_value % 5;
 }
 
 
 void print_hashtable(TableEntry **hashtable, int slot_bitmap[])
 {
+
     TableEntry *tmp = alloc_tableentry(" ", 0);
 
     /* Number of possible hashes is 5. */
+
     for (int i = 0; i < 5; i++) {
+
         cout << "Slot " << i << endl;
+
         /* If there is nothing in the hash table at this index */
+
         if (slot_bitmap[i] == 0) {
+
             continue;
+
         }
 
         tmp = hashtable[i];
@@ -61,27 +82,44 @@ void print_hashtable(TableEntry **hashtable, int slot_bitmap[])
         while (1) {
 
             cout << tmp->data << endl;
+
             tmp = tmp->next;
+
             /* End of the linked list. */
             if (tmp == nullptr) {
+
                 break;
+
             }
+
         }
+
     }
+
     free(tmp);
+
 }
 
 
 TableEntry* alloc_tableentry(string s, int data)
 {
+
     TableEntry *ptr = (struct TableEntry *) malloc(sizeof(struct TableEntry));
+
     if (ptr == nullptr) {
+
         cout << "malloc() has failed. Exiting\n" << endl;
+
     }
+
     ptr->next = nullptr;
+
     ptr->s = s;
+
     ptr->data = data;
+
     return ptr;
+
 }
 
 
@@ -89,9 +127,13 @@ int main(int argc, char *argv[])
 {
 
     TableEntry *a = alloc_tableentry("algorithmicefficiency", 11111);
+
     TableEntry *b = alloc_tableentry("badcode", 22222);
+
     TableEntry *c = alloc_tableentry("g00dc00de", 33333);
+
     TableEntry *d = alloc_tableentry("hexdumps", 44444);
+
     TableEntry *e = alloc_tableentry("d3adc0de", 55555);
 
     TableEntry *tmp = alloc_tableentry(" ", 0);
@@ -109,7 +151,9 @@ int main(int argc, char *argv[])
         hash_value = hash_func(basictable[i]);
 
         /* The slot is open */
+
         if (slot_bitmap[hash_value] == 0) {
+
             cout << "Found open slot at " << hash_value << endl;
 
             hashtable[hash_value] = basictable[i];
@@ -120,14 +164,18 @@ int main(int argc, char *argv[])
 
         }
 
-
         /* Collision */
+
         else {
+
             cout << "Collision at slot " << hash_value << endl;
+
             tmp = hashtable[hash_value];
 
             /* Iterate through linked list at hash */
+
             while (tmp->next != nullptr) {
+
                 cout << "Traversing chained TableEntries" << endl;
 
                 tmp = tmp->next;
@@ -135,7 +183,9 @@ int main(int argc, char *argv[])
             }
 
         /* Link new TableEntry to tail */
+
         cout << "Linking up TableEntry to tail at slot " << hash_value << endl;
+
         tmp->next = basictable[i];
 
         }
@@ -143,18 +193,19 @@ int main(int argc, char *argv[])
     }
 
     /* Check if it worked */
+
     print_hashtable(hashtable, slot_bitmap);
+
     free(a);
+
     free(b);
+
     free(c);
+
     free(d);
+
     free(e);
+
     free(tmp);
 
 }
-
-
-
-
-
-
