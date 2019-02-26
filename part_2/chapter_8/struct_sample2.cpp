d05635d1f6434fb6a17094391b53daf635bbee2d
realbadbytes <badbytes.io@gmail.com>
2019-02-25 23:05:50 -0800

    beginning chapter 8

---
+++ part_2/chapter_8/struct_sample2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+/* User-defined type describing a blog post. */
+struct BlogPost {
+
+    /* post name */
+    const char* name;
+
+    /* post author */
+    const char* author;
+
+    /* post tags i.e. "development" is "d" */
+    char tags[13];
+
+    /* word count */
+    int length;
+} __attribute__ ((packed));
+
+
+int main()
+{
+    BlogPost bp = { 
+        "badbytes.io",
+        "realbadbytes",
+        {'d', 'c', 'n'},
+        542
+    };
+
+    cout << "BlogPost bp size according to sizeof: " << sizeof(bp) << endl;
+
+    return 0;
+}
