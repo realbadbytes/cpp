b5017c123f41b77aeee479e540b5f91a144c6d00
realbadbytes <badbytes.io@gmail.com>
2019-03-11 19:25:10 -0700

    updates to smart ptr example

--- part_2/chapter_11/smart_ptr_1.cpp
+++ part_2/chapter_11/smart_ptr_1.cpp
@@ -4,19 +4,16 @@
 using namespace std;
 
 int main()
-{
+{   
     unique_ptr<int> ptr1 {new int(99)};
 
-    cout << "ptr1 = " << *ptr1 << endl;
+    cout << "ptr1 = " << *ptr1 << " at " << ptr1.get() << endl;
 
-    /* Now we make a new ptr and move the values from old ptr into it
-     * this causes the reference count of ptr1 to become 0, and it
-     * is then garbage collected.
-     */
-
+    /* Transfer ownership to ptr2 */
     unique_ptr<int> ptr2 { move(ptr1) };
 
-    cout << "ptr2 = " << *ptr2 << endl;
+    /* Should be the same address as the old ptr1 */
+    cout << "ptr2 = " << *ptr2 << " at " << ptr2.get() << endl;
 
     /* Causes segfault because ptr1 no longer exists. */
     cout << "ptr1 = " << *ptr1 << endl;
